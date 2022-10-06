04b04a5f967df3a135183c8130b1bf716e914bb0
Akshat Jain <97268578+spiderHash@users.noreply.github.com>
2022-10-06 08:54:14 +0000

    Echo Client Server Using TCP

---
+++ Echo Client Server Using TCP/client.c
@@ -0,0 +1,66 @@
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define MAX 80
+#define PORT 8000
+#define SA struct sockaddr
+void func(int sockfd)
+{
+    char buff[MAX];
+    int n;
+    for (;;) {
+        bzero(buff, sizeof(buff));
+        printf("Enter the string(to end type 'exit') : ");
+        n = 0;
+        while ((buff[n++] = getchar()) != '\n')
+            ;
+            
+            
+        send(sockfd, buff, sizeof(buff),0);
+        bzero(buff, sizeof(buff));
+        recv(sockfd, buff, sizeof(buff),0);
+        printf("Echo From Server : %s", buff);
+        if ((strncmp(buff, "exit", 4)) == 0) {
+            printf("Client Exit...\n");
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int sockfd, connfd;
+    struct sockaddr_in servaddr, cli;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+
+    bzero(&servaddr, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_port = htons(PORT);
+
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
+        printf("connection with the server failed...\n");
+        exit(0);
+    }
+    else
+        printf("connected to the server..\n");
+
+    func(sockfd);
+
+    close(sockfd);
+}
