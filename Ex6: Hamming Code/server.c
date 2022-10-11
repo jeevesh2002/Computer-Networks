88b811e2fae89c875ce1af1dd298dd898ce7f2ea
Akshat Jain <97268578+spiderHash@users.noreply.github.com>
2022-10-11 08:23:20 +0000

    Ex 6: Added Hamming Code

--- Ex6: Hamming Code/server.c
+++ Ex6: Hamming Code/server.c
@@ -1,24 +1,27 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<string.h>
-#include<unistd.h>
-#include<arpa/inet.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <math.h>
+#include<time.h>
 
-#define MAXLINE 1024
+#define MAX 50
+#define PORT 8080
 
-int countbits(long num)
-{
-    int r, count = 0;
-    while(num > 0)
-    {
-        num = num / 10;
-        count++;
-    }
-    return count;
+int find_r(int m){
+    int r=1;
+    while(pow(2, r) < r + m + 1)
+        r += 1;
+    return r;
+}
+int r_position(int n){
+    return (ceil(log2(n))==floor(log2(n)));
 }
 int to_binary(int n){
     int num=0,rem,i=0;
@@ -43,95 +46,105 @@ int ispresent(int num,int pos)
     else
         return 0;
 }
-
-int decimal(int num)
-{
-    int rem, i = 0, result;
-    while(num > 0)
-    {
-        rem = num % 10;
-        result += pow(2, i) * rem;
-        num /= 10;
-        i++;
-    }
-    return result;
-}
-
 int main(int argc, char **argv)
 {
-    int PORT = 8080;
-
-    long num;
-    int sockfd, data_len,rem, code[40], count = 0, r = 0, result = 0,newarr[20], finalarr[20];
-    char buff[40],data_s[50];
-    struct sockaddr_in servaddr;
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket creation failed!");
-        exit(1);
-    }
-    bzero(&servaddr,sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    int n, len;
-    connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
-    n = read(sockfd, buff, sizeof(buff));
-    num = atol(buff);
-    sprintf(data_s,"%lu",num);
-    data_len=strlen(data_s);
-    printf("Received data: %lu\n", num);
-    int i = 1;
-    while(num > 0)
+    int sockfd,newfd, len;
+    struct sockaddr_in server_address, client_address;
+    char buff[30];
+    //socket establishing
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
     {
-        rem = num % 10;
-        code[data_len - i] = rem;
-        num /= 10;
-        i++;
+        perror("SOCKET NOT ESTABLISHED.\n");
+        exit(EXIT_FAILURE);
     }
-    for(int i = 1; i <= data_len; i++)
-        if(ceil(log2(i)) == floor(log2(i)))
-            r++;
-    int k = 0,binary ;
-    for(int i = 0; i < 4; i++)
+    //clear address pointer
+    bzero(&server_address, sizeof(server_address));
+    // assign IP, PORT
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(PORT);
+    //bind the socket with the IP
+    if ((bind(sockfd, (const struct sockaddr *)&server_address, sizeof(server_address))) != 0)
+    {
+        perror("Bind error occurred.\n");
+        exit(EXIT_FAILURE);
+    }
+    listen(sockfd, 2);
+    //start
+    long data;
+    char data_s[MAX];
+    int code[MAX];
+    printf("Enter input data: ");
+    scanf("%lu",&data);
+    sprintf(data_s,"%lu",data);
+    int m=strlen(data_s);
+    int r=find_r(m);
+    printf("\nNumber of redundant bits: %d",r);
+    int data_len=m+r;
+    printf("\nNumber of bits in code: %d",data_len);
+    int digit;
+    for(int i=1;i<=data_len;i++){
+        digit=data%10;
+        if(r_position(i)){
+            code[data_len-i]=2;
+        }
+        else{
+            code[data_len-i]=digit;
+            data=data/10;
+        }
+    }
+    /*printf("\n");
+    for(int i=0;i<data_len;i++){
+        printf("%d",code[i]);
+    }
+    printf("\n");*/
+    int binary,count=0;
+    for(int i = 0; i < r; i++)
     {
         for(int j = 1; j <= data_len; j++)
         {
-            binary = to_binary(j);
-            if(ispresent(binary, i + 1))
-                count += code[data_len - j];
+            if((int)(pow(2, i)) != j)
+            {
+                binary = to_binary(j);
+                //printf("%d",bin);
+                if(ispresent(binary, i + 1))
+                    count += code[data_len - j];
+            }
+            //printf("%d",j);
         }
+        //check for even parity
         if(count % 2 == 0)
-            result += pow(10, k) * 0;
+            code[data_len - (int)(pow(2, i))] = 0;
         else
-            result += pow(10, k) * 1;
-        k++;
-        count=0;
+            code[data_len - (int)(pow(2, i))] = 1;
+        count = 0;
+        //printf("\n%d\n",i);
     }
-    int error = decimal(result);
-    printf("\nError bit in binary: %d\n", result);
-    printf("\nError in bit %d\n", error);
-    if(code[data_len - error] == 0)
-        code[data_len - error] = 1;
-    else
-        code[data_len - error] = 0;
-    k = 0;
-    printf("\nData after error correction: ");
-    for(int i = data_len - 1; i >= 0; i--)
-    {
-        newarr[k] = code[i];
-        k++;
-    }
-    int x = 0;
-    for(int i = 0;i < k; i++)
-    {
-        if(ceil(log2(i + 1)) != floor(log2(i + 1)))
-        {
-            finalarr[x] = newarr[i];
-            x++;
-        }
-    }
-    for(int i = x - 1; i >= 0; i--)
-        printf("%d", finalarr[i]);
+    printf("\nData with redundant bits: ");
+    for(int i = 0; i < data_len; i++)
+        printf("%d", code[i]);
     printf("\n");
-    return 0;
+    //introduce error
+    int pos = rand() % data_len + 1;
+    printf("\nIntroducing error automatically at bit: %d\n", pos);
+    if(code[data_len - pos] == 0)
+        code[data_len - pos] = 1;
+    else
+        code[data_len - pos] = 0;
+    int k = 0;
+    long num = 0;
+    for(int i = data_len-1; i >= 0; i--)
+    {
+        num += pow(10, k) * code[i];
+        k++;
+    }
+    sprintf(data_s, "%lu", num);
+    printf("Data transmitted is %s\n", data_s);
+    //stop
+
+    len = sizeof(client_address);
+    newfd = accept(sockfd, (struct sockaddr*)&client_address, &len);
+    m = write(newfd, data_s, sizeof(data_s));
+    close(sockfd);
 }
